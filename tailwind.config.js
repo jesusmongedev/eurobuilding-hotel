d6a360d868f260003371883aa1a49cdadb25f3ba
Jesús Monge <jesus_jaml@outlook.com>
2022-01-20 18:59:29 -0500

    Fixing the RoomSlider's scroll-snap-type and delete a tailwind plugin dependecy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./Components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './Components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        primary: "#4133FF",
+        primary: '#4133FF',
       },
     },
   },
-  plugins: [require("tailwind-scrollbar")],
-};
+  plugins: [],
+}
