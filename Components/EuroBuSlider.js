43493ab34470cbd5ed63ceebff8a44ace6e47bb3
Jesús Monge <jesus_jaml@outlook.com>
2022-01-27 01:26:00 -0500

    EuroBuSlider using next/img V_0.2 Done mobile first ✅

--- Components/EuroBuSlider.js
+++ Components/EuroBuSlider.js
@@ -1,8 +1,20 @@
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa'
 
 const EuroBuSlider = () => {
   const [sliderImage, setSliderImage] = useState([])
+  const [current, setCurrent] = useState(0)
+  const sliderImageLength = sliderImage?.length
+
+  const nextSlide = () => {
+    setCurrent(current === sliderImageLength - 1 ? 0 : current + 1)
+  }
+  const prevSlide = () => {
+    setCurrent(current === 0 ? sliderImageLength - 1 : current - 1)
+  }
+
+  console.log(current)
 
   useEffect(() => {
     window
@@ -15,16 +27,37 @@ const EuroBuSlider = () => {
   }, [])
 
   return (
-    <div className="relative h-[300px] flex items-center justify-center">
+    <div className="relative flex items-center justify-center">
+      <FaArrowAltCircleLeft
+        className="slider-arrow left-[15px]"
+        onClick={prevSlide}
+      />
+      <FaArrowAltCircleRight
+        className="slider-arrow right-[15px]"
+        onClick={nextSlide}
+      />
       {sliderImage?.map((slide, index) => {
         return (
-          <Image
+          <div
             key={index}
-            src={slide?.room_image.url}
-            width={600}
-            height={400}
-            alt={slide?.room_image.alt}
-          />
+            className={
+              index === current
+                ? 'opacity-1 duration-1000 scale-105'
+                : 'opacity-0 duration-1000 ease'
+            }
+          >
+            {index === current && (
+              // eslint-disable-next-line @next/next/no-img-element
+              <Image
+                src={slide?.room_image.url}
+                alt={slide?.room_image.alt}
+                width={305}
+                height={240}
+                objectFit="contain"
+                className="rounded-lg"
+              />
+            )}
+          </div>
         )
       })}
     </div>
