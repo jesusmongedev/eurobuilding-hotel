dbd80f9411b4423e954b4492eb7840c60530b0d7
Jesús Monge <jesus_jaml@outlook.com>
2022-02-08 23:28:43 -0500

    Opiniones component builded, add data to the opinios db

--- Components/Opiniones.js
+++ Components/Opiniones.js
@@ -1,23 +1,70 @@
-import { setDayOfYear } from 'date-fns'
+import Image from 'next/image'
 import { useEffect, useState } from 'react'
+import { AiFillStar } from 'react-icons/ai'
 
 const Opiniones = () => {
   const [opinions, setOpinions] = useState([])
+  const [current, setCurrent] = useState(0)
+
+  const star = (star) => {
+    var stars = []
+    for (var i = 0; i < star; i++) {
+      stars.push(<AiFillStar className="text-yellow-400" />)
+    }
+    if (star < 5) {
+      for (var i = 0; i < 5 - star; i++) {
+        stars.push(<AiFillStar className="text-gray-400 " />)
+      }
+    }
+    return stars
+  }
+
   useEffect(() => {
     window
       .fetch('/api/opinions')
       .then((res) => res.json())
       .then(({ opinions }) => {
-        console.log(opinions)
         setOpinions(opinions)
       })
   }, [])
   return (
-    <div>
-      <p>Opiniones</p>
-      {opinions?.map((opinion, i) => (
-        <p key={i}>{opinion.name}</p>
-      ))}
+    <div className="bg-gray-200 px-4 pt-4 pb-2">
+      <h2 className="text-sm font-semibold mb-2">OPINIONES</h2>
+      <p className="text-xs">{opinions[current]?.decription}</p>
+      <div className="flex gap-2 mt-3 mb-2 items-center">
+        <Image
+          src={opinions[current]?.image}
+          width={40}
+          height={40}
+          objectFit="contain"
+          alt={opinions[current]?.name}
+          className="rounded-full"
+        />
+        <div className="space-y-2">
+          <h3 className="text-sm font-semibold">{opinions[current]?.name}</h3>
+          <div className="flex gap-1">{star(opinions[current]?.star)}</div>
+        </div>
+      </div>
+      <div className="text-center space-x-2">
+        <button
+          onClick={() => {
+            setCurrent(0)
+          }}
+          className="rounded-full border border-gray-400 w-3 h-3"
+        ></button>
+        <button
+          onClick={() => {
+            setCurrent(1)
+          }}
+          className="rounded-full border border-gray-400 w-3 h-3"
+        ></button>
+        <button
+          onClick={() => {
+            setCurrent(2)
+          }}
+          className="rounded-full border border-gray-400 w-3 h-3"
+        ></button>
+      </div>
     </div>
   )
 }
